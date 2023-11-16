400bcee12fb47e33a50212b952414da5ebf9e7d7
Evaisa <evagiacosa1@gmail.com>
2023-11-16 17:14:46 +0100

    wahhh why it no work

--- LethalLib/Modules/Items.cs
+++ LethalLib/Modules/Items.cs
@@ -18,37 +18,18 @@ namespace LethalLib.Modules
     {
         public static void Init()
         {
-            cancelPurchaseNode = ScriptableObject.CreateInstance<TerminalNode>();
-
-            /*
-            new TerminalNode()
-            {
-                displayText = "Cancelled order",
-                clearPreviousText = true,
-                maxCharactersToType = 35,
-                buyItemIndex = -1,
-                isConfirmationNode = false,
-                itemCost = 0,
-            };
-            */
-            cancelPurchaseNode.name = "LethalLibCancelPurchaseNode";
-            cancelPurchaseNode.displayText = "Cancelled order";
-            cancelPurchaseNode.clearPreviousText = true;
-            cancelPurchaseNode.maxCharactersToType = 35;
-            cancelPurchaseNode.buyItemIndex = -1;
-            cancelPurchaseNode.isConfirmationNode = false;
-            cancelPurchaseNode.itemCost = 0;
-
             On.StartOfRound.Awake += RegisterLevelScrap;
             On.Terminal.Awake += Terminal_Awake;
         }
 
-        public static TerminalNode cancelPurchaseNode;
 
         private static void Terminal_Awake(On.Terminal.orig_Awake orig, Terminal self)
         {
             var terminal = UnityEngine.Object.FindObjectOfType<Terminal>();
             var itemList = terminal.buyableItemsList.ToList();
+
+            var buyKeyword = self.terminalNodes.allKeywords.First(keyword => keyword.word == "buy");
+            var cancelPurchaseNode = buyKeyword.compatibleNouns[0].result.terminalOptions[1].result;
 
             Plugin.logger.LogInfo($"Adding {shopItems.Count} items to terminal");
             foreach (ShopItem item in shopItems)
@@ -59,62 +40,46 @@ namespace LethalLib.Modules
                 var lastChar = itemName[itemName.Length - 1];
                 var itemNamePlural = itemName;
 
-                if (lastChar == 's')
-                {
-                    itemNamePlural += "es";
-                }
-                else
-                {
-                    itemNamePlural += "s";
-                }
-
                 var buyNode2 = ScriptableObject.CreateInstance<TerminalNode>();
 
-                buyNode2.name = $"{itemName}BuyNode2";
+                buyNode2.name = $"{itemName.Replace(" ", "-")}BuyNode2";
                 buyNode2.displayText = $"Ordered [variableAmount] {itemNamePlural}. Your new balance is [playerCredits].\n\nOur contractors enjoy fast, free shipping while on the job! Any purchased items will arrive hourly at your approximate location.";
                 buyNode2.clearPreviousText = true;
                 buyNode2.maxCharactersToType = 15;
-                buyNode2.buyItemIndex = itemList.Count;
-                buyNode2.isConfirmationNode = true;
+                buyNode2.buyItemIndex = itemList.Count - 1;
+                buyNode2.isConfirmationNode = false;
                 buyNode2.itemCost = item.price;
-                
-                /*
-                var buyNode1 = new TerminalNode()
-                {
-                    displayText = $"You have requested to order {itemNamePlural}. Amount: [variableAmount].\nTotal cost of items: [totalCost].\n\nPlease CONFIRM or DENY.\r\n\r\n",
-                    clearPreviousText = true,
-                    maxCharactersToType = 35,
-                    buyItemIndex = itemList.Count,
-                    isConfirmationNode = false,
-                    itemCost = item.price,
-                };*/
+               
 
-            var buyNode1 = ScriptableObject.CreateInstance<TerminalNode>();
-                buyNode1.name = $"{itemName}BuyNode1";
+                var buyNode1 = ScriptableObject.CreateInstance<TerminalNode>();
+                buyNode1.name = $"{itemName.Replace(" ", "-")}BuyNode1";
                 buyNode1.displayText = $"You have requested to order {itemNamePlural}. Amount: [variableAmount].\nTotal cost of items: [totalCost].\n\nPlease CONFIRM or DENY.\r\n\r\n";
                 buyNode1.clearPreviousText = true;
                 buyNode1.maxCharactersToType = 35;
-                buyNode1.buyItemIndex = itemList.Count;
-                buyNode1.isConfirmationNode = false;
+                buyNode1.buyItemIndex = itemList.Count - 1;
+                buyNode1.isConfirmationNode = true;
+                buyNode1.overrideOptions = true;
                 buyNode1.itemCost = item.price;
-
-                buyNode1.terminalOptions.AddToArray(new CompatibleNoun()
+                buyNode1.terminalOptions = new CompatibleNoun[2]
                 {
-                    noun = self.terminalNodes.allKeywords.First(keyword => keyword.word == "confirm"),
-                    result = buyNode2
-                });
+                    new CompatibleNoun()
+                    {
+                        noun = self.terminalNodes.allKeywords.First(keyword => keyword.word == "confirm"),
+                        result = buyNode2
+                    },
+                    new CompatibleNoun()
+                    {
+                        noun = self.terminalNodes.allKeywords.First(keyword => keyword.word == "deny"),
+                        result = cancelPurchaseNode
+                    }
+                };
+                TerminalKeyword keyword = TerminalUtils.CreateTerminalKeyword(itemName.ToLowerInvariant().Replace(" ", "-"), defaultVerb: buyKeyword);
 
-                buyNode1.terminalOptions.AddToArray(new CompatibleNoun()
-                {
-                    noun = self.terminalNodes.allKeywords.First(keyword => keyword.word == "deny"),
-                    result = cancelPurchaseNode
-                });
+                //self.terminalNodes.allKeywords.AddItem(keyword);
+                var allKeywords = self.terminalNodes.allKeywords.ToList();
+                allKeywords.Add(keyword);
+                self.terminalNodes.allKeywords = allKeywords.ToArray();
 
-                TerminalKeyword keyword = TerminalUtils.CreateTerminalKeyword(itemName.ToLowerInvariant().Replace(" ", "-"), defaultVerb: self.terminalNodes.allKeywords.First(keyword => keyword.word == "buy"));
-
-                self.terminalNodes.allKeywords.AddItem(keyword);
-
-                var buyKeyword = self.terminalNodes.allKeywords.First(keyword => keyword.word == "buy");
                 var nouns = buyKeyword.compatibleNouns.ToList();
                 nouns.Add(new CompatibleNoun()
                 {
--- LethalLib/Plugin.cs
+++ LethalLib/Plugin.cs
@@ -66,7 +66,5 @@ namespace LethalLib
 
 
 
-
-
     }
 }
